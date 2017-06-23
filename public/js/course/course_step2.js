61764e66a1df923ca67acf4203f9a5569de07053
tomsj <1084851170@qq.com>
2017-06-23 14:13:22 +0800

    图片的上传和裁切功能完成了

--- public/js/course/course_step2.js
+++ public/js/course/course_step2.js
@@ -1,11 +1,9 @@
-define(['jquery', 'template', 'uploadify', 'jcrop'], function ($, template) {
+define(['jquery', 'template', 'layer', 'uploadify', 'jcrop'], function ($, template, layer) {
 
   var cs_id = getQueryString('cs_id');
 
-
   // 渲染页面
   render();
-
 
   // 渲染页面函数
   function render () {
@@ -20,12 +18,82 @@ define(['jquery', 'template', 'uploadify', 'jcrop'], function ($, template) {
         if ( data.code ) {
           var html = template('pic_tpl', data.result);
           $('.steps').html(html);
-            // 上传图片
-            upload();
+          // 上传图片
+          upload();
+          if ( data.result.cs_cover_original ) {
+            $("#jcrop_btn").prop('disabled', false);
+          }
+          
+          //剪裁图片
+          cutPic();
         }
       }
     });
-  } 
+  }
+  
+  // 剪裁图片的函数
+  function cutPic () {
+    var jcrop_api;
+    // 给裁切按钮注册单击事件
+    $("#jcrop_btn").on('click', function () {
+      // 判断按钮的值
+      switch ( $(this).val() ) {
+        case '裁切图片':
+          $(this).val('保存图片');
+          $(".upfile_pic_perview > img").Jcrop({
+            boxWidth: 400,
+            aspectRatio: 2
+          }, function () {
+            jcrop_api = this;
+            // 新建一个缩略图，修改了一下源码thumb:'.thumb'，添加的一个属性， thumb属性是将
+            // 缩略图放到指定的位置。
+            this.initComponent('Thumbnailer', {width: 240, height: 120, thumb:'.thumb'});
+            this.newSelection();
+            var height = this.ui.stage.height - 100;
+            var width = this.ui.stage.width;
+            this.setSelect([0, 50, width, height]);
+          });
+          
+          break;
+        case '保存图片':
+          // 发送ajax请求，将坐标发送
+          var x = $('#cropx').val(),
+            y = $('#cropy').val(),
+            w = $('#cropw').val(),
+            h = $('#croph').val();
+            
+          $.ajax({
+            url: '/api/course/update/picture',
+            type: 'post',
+            dataType: 'json',
+            data: {
+              cs_id: cs_id,
+              x: x,
+              y: y,
+              w: w,
+              h: h
+            },
+            success: function ( data ) {
+              // console.log(data);
+              if ( data.code === 200 ) {
+                layer.msg("剪裁成功");
+                setTimeout(function () {
+                  location.href = "../course/course_add_step3"
+                }, 1500);
+              }
+            }
+          });
+          break;
+      }
+    });
+    
+    $(".upfile_pic_perview").on('cropstart cropmove cropend', function (e, s, c) {
+      $('#cropx').val(c.x);
+      $('#cropy').val(c.y);
+      $('#cropw').val(c.w);
+      $('#croph').val(c.h);
+    });
+  }
   
   // 上传图片的函数
   function upload() {
@@ -36,7 +104,6 @@ define(['jquery', 'template', 'uploadify', 'jcrop'], function ($, template) {
       width: 70,
       swf: '/public/assets/uploadify/uploadify.swf',
       uploader: '/api/uploader/cover',
-
       method: 'post',
       buttonClass: 'btn btn-success btn-sm', 
       buttonText: '选择图片',
@@ -63,43 +130,19 @@ define(['jquery', 'template', 'uploadify', 'jcrop'], function ($, template) {
         $('.upfile_pic_perview').empty().html('<img src='+ path +' />');
         // 让按钮可用
         $('#jcrop_btn').prop('disabled', false);
-         //裁切图片
-         var jcrop_api;
-         $(".upfile_pic_perview img").Jcrop({
-           aspectRatio: 2 ,
-           setSelect: [ 175, 100, 400, 300 ],
-           boxWidth: 400
-         }, function () {
-            jcrop_api = this;
-            this.initComponent('Thumbnailer', { width: 240, height: 120, thumb: '.thumb'});
-
-         });
-
-       
-
-         picCut();
-
-        //  预览裁切
-
+        // 裁切图片
+        $("#jcrop_btn").val('裁切图片');
+        
       }
     });  
   }
 
-
-
-  // 图片裁切
-  function picCut() {
-    
-    $(".steps").on('click', '#jcrop_btn', function () {
-        alert("保存图片");
-    });
-   
-  }
-
-     // 获取到 url 参数的函数
+  
+  // 获取到 url 参数的函数
   function getQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return decodeURIComponent(r[2]); return null;
   }
+  
 });
